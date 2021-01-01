3c9ed3b483663109573bc13f4caa19edb830f090
Bilboking <brkbendreality@gmail.com>
2021-01-01 09:05:18 -0600

    01-01-2021

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+    <p ref="p">My name is {{ name }} and my age is {{ age }}.</p>
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    console.log('setup')
+
+    const p = ref(null)
+
+    let name ='mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+    }
+
+    return { name, age, handleClick, p }
   }
 }
 </script>
